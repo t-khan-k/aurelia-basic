282281818311f14bb49633555c1307cecb3fd491
tkhank <tkssuetce@live.com>
2016-03-14 23:40:49 +0500

    init

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
+      {route: ['users'], name:'users', moduleId: './users', nav: true, title: 'Hello Users'}
+    ]);
+
+    this.router = router;
+  }
+}
